099edbe0dc34263b4783e612fe3e84526500a7d5
Arseniy Tashoyan <tashoyan@gmail.com>
2020-11-15 02:11:43 +0300

    WIP: Submit script

--- spark-project-template/src/main/resources/bin/dummy-spark-submit.sh
+++ spark-project-template/src/main/resources/bin/dummy-spark-submit.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-#TODO
+# TODO log config
+# TODO Spark config - via file
+# TODO fat jar is in lib/
+spark2-submit \
+  --master yarn \
+  --deploy-mode cluster \
+  --name Dummy \
+  --class org.fivt.atp.bigdata.DummyMain \
+  @{project.artifactId}-@{project.version}.jar
