11f2e4c05bbaef70014cfc595717d5d47acd8413
Arseniy Tashoyan <tashoyan@gmail.com>
2020-11-15 00:16:28 +0300

    Add dummy test. Disable default Java compile and test executions, as there is no Java code.

---
+++ src/test/scala/org/fivt/atp/bigdata/DummySpec.scala
@@ -0,0 +1,19 @@
+package org.fivt.atp.bigdata
+
+import org.apache.spark.sql.SparkSession
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class DummySpec extends AnyFlatSpec with Matchers {
+  // TODO Log configuration - by default Spark is very verbose
+  private val spark = SparkSession.builder()
+    .master("local[2]")
+    .appName(this.getClass.getSimpleName)
+    .getOrCreate()
+
+  "A test" should "access Spark version" in {
+    println(s"Spark version: ${spark.version}")
+    spark.version should not be empty
+  }
+
+}
