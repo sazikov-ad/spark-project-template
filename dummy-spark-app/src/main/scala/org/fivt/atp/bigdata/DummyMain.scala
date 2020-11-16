4f2d9aed0b819e0ed1da089c5dc8ceb93d3db623
Arseniy Tashoyan <tashoyan@gmail.com>
2020-11-16 16:24:55 +0300

    Make the app running longer to see it in Spark UI

--- dummy-spark-app/src/main/scala/org/fivt/atp/bigdata/DummyMain.scala
+++ dummy-spark-app/src/main/scala/org/fivt/atp/bigdata/DummyMain.scala
@@ -1,6 +1,7 @@
 package org.fivt.atp.bigdata
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object DummyMain {
 
@@ -8,9 +9,20 @@ object DummyMain {
     val spark = SparkSession.builder()
       .getOrCreate()
     import spark.implicits._
-    val data = spark.createDataset(0 until 10)
+
+    val data = spark.createDataset(0 until 1000000)
       .toDF("value")
+    println("Data sample:")
     data.show(false)
+
+    val count = data.count()
+    println(s"The data has $count values in total")
+
+    val modulo2Data = data.withColumn("modulo2", col("value") % 2)
+      .groupBy("modulo2")
+      .count()
+    println("Even/odd numbers:")
+    modulo2Data.show(false)
   }
 
 }
